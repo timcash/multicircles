c02ac0b570ae3b116c6bc6bdf06082502e01e34b
Tim Cash <timcash@gmail.com>
2016-01-14 22:23:19 -0800

    colors

--- js/index.js
+++ js/index.js
@@ -1,42 +1,61 @@
 import L from "lodash/fp";
 
-console.log(L.range(0,5));
-
 let pi2 = 2 * Math.PI;
-let canvas = document.getElementById('draw');
-let ctx = canvas.getContext('2d');
-ctx.fillStyle = "rgb(200,0,0)";
+let csize = 300;
+let nodes = 360;
 
-let nodes = 10;
-let mult  = 2;
-
-let c = genCircle(nodes, 150);
-log(c.length);
-let c1  = L.take(c.length - 1, c);
-let c2  = L.takeRight(c.length - 1, c);
-let z   = L.zip(c1, c2);
-let p1  = L.map((x)=> [x, x * mult % nodes], L.range(0,nodes));
-let p2  = L.map((x)=> [c[x[0]], c[x[1]]], p1);
-log(p2);
-
-
-ctx.beginPath();
-L.map((p)=> line(ctx, p[0], p[1]), p2);
-ctx.stroke();
-
-ctx.beginPath();
-ctx.arc(150, 150, 150, 0, pi2);
-ctx.stroke();
+L.map(makeBox, L.range(2, nodes+1));
 
 function log(t) {
     console.log(JSON.stringify(t, true, 4));
 }
 
+function makeBox(i) {
+    log("make a thing");
+    let canvas = document.createElement('canvas');
+    let cid = document.createTextNode(`${i}`);
+    canvas.setAttribute("id", `canvas${i}`);
+    canvas.width = csize;
+    canvas.height = csize;
+    document.body.appendChild(cid);
+    document.body.appendChild(canvas);
+
+    let ctx = canvas.getContext('2d');
+
+    let mult  = i;
+
+    let c = genCircle(nodes, csize/2);
+    let c1  = L.take(c.length - 1, c);
+    let c2  = L.takeRight(c.length - 1, c);
+    let z   = L.zip(c1, c2);
+    let p1  = L.map((x)=> [x, x * mult % nodes], L.range(0,nodes));
+    let p2  = L.map((x)=> [c[x[0]], c[x[1]]], p1);
+
+    let hue = 101;
+    let sat = 100;
+    let dir = 1;
+    let lgt = 70;
+    L.map((p)=> {
+        hue += 1.3 * dir;
+        sat -= 0.07;
+        lgt -= 0.07;
+
+        if (hue <= 50 || hue >= 190) dir = -dir;
+        ctx.beginPath();
+        ctx.strokeStyle = `hsl(${hue}, ${sat}%, ${lgt}%)`;
+
+        line(ctx, p[0], p[1]);
+        ctx.stroke();
+    }, p2);
+
+    ctx.beginPath();
+    ctx.arc(csize/2, csize/2, csize/2, 0, pi2);
+    ctx.stroke();
+}
+
 function main() {
     return 0;
 }
-
-console.log("hello");
 
 function line(ctx, p1, p2) {
     ctx.moveTo(p1[0],p1[1]);
@@ -46,8 +65,8 @@ function line(ctx, p1, p2) {
 function circlePoint(r, n, i) {
 
     let theta = (i/n) * pi2;
-    let px = Math.cos(theta) * r + 150;
-    let py = Math.sin(theta) * r + 150;
+    let px = Math.cos(theta) * r + csize/2;
+    let py = Math.sin(theta) * r + csize/2;
     return [px, py];
 }
 
